a1a1076b6130bc7c8a436f12a2dcad7694d8f254
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-05-16 08:38:59 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
-#
+from pathlib import Path
+
 # url = "https://dvmn.org/filer/canonical/1542890876/16/"
 #
 # responce = requests.get(url)
@@ -9,12 +10,16 @@ import requests
 # with open (file_name, 'wb') as file:
 #     file.write(responce.content)
 
+Path("books").mkdir(parents=True, exist_ok=True)
+for book_number in range (32170, 32180):
+    url = f"https://tululu.org/txt.php?id={book_number}"
+    # print(url)
 
-url = "https://tululu.org/txt.php?id=32168"
+    responce = requests.get(url)
+    responce.raise_for_status()
+# https://tululu.org/txt.php?id=32168
 
-responce = requests.get(url)
-responce.raise_for_status()
 
-file_name = 'sands_of_mars.txt'
-with open (file_name, 'wb') as file:
-    file.write(responce.content)
+    file_name = f'books/{book_number}.txt'
+    with open (file_name, 'wb') as file:
+        file.write(responce.content)
