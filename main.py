30523531b898c96c8b6abb9d956f95431757f27b
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-05-19 20:01:36 +0300

    added argparse arguments for command line

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from bs4 import BeautifulSoup
 import os
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
+import argparse
+
+parser = argparse.ArgumentParser(description="Книги в каком диапазоне надо скачать")
+parser.add_argument('-s', '--start_id', help='Начальный номер книги на сайте',
+                    default='1', type=int)
+parser.add_argument('-e', '--end_id', help='Конечный номер книги на сайте',
+                    default='5', type=int)
+args = parser.parse_args()
 
 
 def parse_book_page(content):
@@ -14,14 +22,13 @@ def parse_book_page(content):
     genres = [genre.text for genre in soup.find('span', class_='d_book').find_all('a')]
 
     book = {
-            'Заголовок: ': title,
-            'Автор: ': author,
-            'Жанр: ': genres,
+        'Заголовок: ': title,
+        'Автор: ': author,
+        'Жанр: ': genres,
     }
     comments = soup.find_all(class_='texts')
     for comment in comments:
-        book['Комментарии: ']= comment.text.split(')')[1]
-
+        book['Комментарии: '] = comment.text.split(')')[1]
 
     return book
 
@@ -36,9 +43,9 @@ def download_txt(content, id=1, folder='books/'):
     response_book_download = requests.get(url_book_download, params=payload)
     response_book_download.raise_for_status()
 
-    file_name = f'{id}' + '. ' + sanitize_filename(title)+".txt"
+    file_name = f'{id}' + '. ' + sanitize_filename(title) + ".txt"
 
-    with open (os.path.join(folder, file_name), 'wb') as file:
+    with open(os.path.join(folder, file_name), 'wb') as file:
         file.write(response_book_download.content)
 
 
@@ -48,9 +55,8 @@ def download_image(content, id=1, folder='images/'):
     response = requests.get(urljoin('https://tululu.org/', image))
     response.raise_for_status()
 
-
     file_name = f'{id}' + ".jpg"
-    with open (os.path.join(folder, file_name), 'wb') as file:
+    with open(os.path.join(folder, file_name), 'wb') as file:
         file.write(response.content)
 
 
@@ -64,7 +70,7 @@ def check_for_redirect(response):
 Path("books").mkdir(parents=True, exist_ok=True)
 Path("images").mkdir(parents=True, exist_ok=True)
 
-for book_number in range(1,11):
+for book_number in range(args.start_id, args.end_id + 1):
     payload = {'id': book_number}
     url_book = "https://tululu.org/b"
     url_book_download = "https://tululu.org/txt.php"
@@ -80,9 +86,6 @@ for book_number in range(1,11):
     except requests.exceptions.HTTPError:
         continue
 
-
-    # print(parse_book_page(response_book))
-
+    print(parse_book_page(response_book))
     download_txt(response_book, book_number)
-    # download_image(response_book, book_number)
-
+    download_image(response_book, book_number)
