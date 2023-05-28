7669fe1e8ed1a6171ac3562b6c2bd29ce1b5043d
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-05-28 16:27:28 +0300

    corrections after review

--- main.py
+++ main.py
@@ -5,90 +5,109 @@ import os
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
 import argparse
-
-parser = argparse.ArgumentParser(description="Книги в каком диапазоне надо скачать")
-parser.add_argument('-s', '--start_id', help='Начальный номер книги на сайте',
-                    default='1', type=int)
-parser.add_argument('-e', '--end_id', help='Конечный номер книги на сайте',
-                    default='5', type=int)
-args = parser.parse_args()
+import sys
+from time import sleep
 
 
-def parse_book_page(content):
-    soup = BeautifulSoup(content.text, 'lxml')
-    title_tag = soup.find('h1')
-    title = title_tag.text.strip().replace('\xa0 ', '').split(' :: ')[0]
-    author = title_tag.text.strip().replace('\xa0 ', '').split(' :: ')[1]
-    genres = [genre.text for genre in soup.find('span', class_='d_book').find_all('a')]
+def main():
 
-    comments = soup.find_all(class_='texts')
-    comments_list = []
-    for comment in comments:
-        comments_list.append(comment.text.split(')')[1])
+    class raise_redirect(requests):
+        def check_for_redirect(self):
+            if self.history: raise requests.exceptions.HTTPError
+            pass
 
-    book = {
-        'Заголовок: ': title,
-        'Автор: ': author,
-        'Жанр: ': genres,
-        'Комментарии: ': comments_list
-    }
+    parser = argparse.ArgumentParser(description='Книги в каком диапазоне надо скачать')
+    parser.add_argument('-s', '--start_id', help='Начальный номер книги на сайте',
+                        default='1', type=int)
+    parser.add_argument('-e', '--end_id', help='Конечный номер книги на сайте',
+                        default='5', type=int)
+    args = parser.parse_args()
 
-    return book
+    def parse_book_page(content):
+        soup = BeautifulSoup(content.text, 'lxml')
+        image = soup.find(class_='bookimage').find('img')['src']
+        title_tag = soup.find('h1')
+        title = title_tag.text.strip().replace('\xa0 ', '').split(' :: ')[0]
+        author = title_tag.text.strip().replace('\xa0 ', '').split(' :: ')[1]
+        genres = [genre.text for genre in soup.find('span', class_='d_book').find_all('a')]
+
+        comments_parsed = soup.find_all(class_='texts')
+
+        comments = [comment.text.split(')')[1] for comment in comments_parsed]
+
+        book = {
+            'title: ': title,
+            'author: ': author,
+            'genre: ': genres,
+            'comments: ': comments,
+            'image_url: ': image
+        }
+
+        return book
+
+    def download_txt(title, id=1, folder='books/'):
+        file_name = f'{id}. {sanitize_filename(title)}.txt'
+
+        with open(os.path.join(folder, file_name), 'wb') as file:
+            file.write(response_book_downloading.content)
+
+    def download_image(image, id=1, folder='images/'):
+        response = requests.get(urljoin('https://tululu.org/', image))
+        response.raise_for_status()
+
+        file_name = f'{id}.jpg'
+        with open(os.path.join(folder, file_name), 'wb') as file:
+            file.write(response.content)
+
+    # def check_for_redirect(response):
+    #     if response.history: raise requests.exceptions.HTTPError
+    #     pass
+
+    Path('books').mkdir(parents=True, exist_ok=True)
+    Path('images').mkdir(parents=True, exist_ok=True)
+
+    for book_number in range(args.start_id, args.end_id + 1):
+        payload = {'id': book_number}
+        book_url = 'https://tululu.org/b'
+        book_download_url = 'https://tululu.org/txt.php'
+
+        while True:
+            try:
+                response_book = requests.get(f'{book_url}{book_number}/', allow_redirects=False)
+                response_book.raise_for_status()
+                break
+            except requests.exceptions.ConnectionError:
+                sleep(5)
+                print("Ошибка соединения", file=sys.stderr)
+
+        response_book = raise_redirect.get(f'{book_url}{book_number}/', allow_redirects=False)
+
+        try:
+            response_book.check_for_redirect()
+        except requests.exceptions.HTTPError:
+            print("Страницы не существует", file=sys.stderr)
+            continue
+
+        try:
+            response_book.check_for_redirect()
+        except requests.exceptions.HTTPError:
+            print("Нет книги для скачивания на сайте", file=sys.stderr)
+            continue
+
+        response_book_downloading = raise_redirect.get(book_download_url, params=payload)
+        response_book_downloading.raise_for_status()
+
+        try:
+            response_book_downloading.raise_redirect()
+        except requests.exceptions.HTTPError:
+            print("Нет книги для скачивания на сайте", file=sys.stderr)
+            continue
+
+        title_downloaded = parse_book_page(response_book)
+        download_txt(title_downloaded['title: '], book_number)
+        image_downloaded = parse_book_page(response_book)
+        download_image(image_downloaded['image_url: '], book_number)
 
 
-def download_txt(content, id=1, folder='books/'):
-    soup = BeautifulSoup(content.text, 'lxml')
-    title_tag = soup.find('h1')
-    title = title_tag.text.strip().replace('\xa0 ', '').split(' :: ')[0]
-    payload = {'id': book_number}
-    url_book_download = "https://tululu.org/txt.php"
-
-    response_book_download = requests.get(url_book_download, params=payload)
-    response_book_download.raise_for_status()
-
-    file_name = f'{id}' + '. ' + sanitize_filename(title) + ".txt"
-
-    with open(os.path.join(folder, file_name), 'wb') as file:
-        file.write(response_book_download.content)
-
-
-def download_image(content, id=1, folder='images/'):
-    soup = BeautifulSoup(content.text, 'lxml')
-    image = soup.find(class_='bookimage').find('img')['src']
-    response = requests.get(urljoin('https://tululu.org/', image))
-    response.raise_for_status()
-
-    file_name = f'{id}' + ".jpg"
-    with open(os.path.join(folder, file_name), 'wb') as file:
-        file.write(response.content)
-
-
-def check_for_redirect(response):
-    if not response.history:
-        pass
-    else:
-        raise requests.exceptions.HTTPError
-
-
-Path("books").mkdir(parents=True, exist_ok=True)
-Path("images").mkdir(parents=True, exist_ok=True)
-
-for book_number in range(args.start_id, args.end_id + 1):
-    payload = {'id': book_number}
-    url_book = "https://tululu.org/b"
-    url_book_download = "https://tululu.org/txt.php"
-
-    response_book = requests.get(f'{url_book}{book_number}')
-    response_book.raise_for_status()
-
-    response_book_download = requests.get(url_book_download, params=payload)
-    response_book_download.raise_for_status()
-
-    try:
-        check_for_redirect(response_book_download)
-    except requests.exceptions.HTTPError:
-        continue
-
-    print(parse_book_page(response_book))
-    download_txt(response_book, book_number)
-    download_image(response_book, book_number)
+if __name__ == '__main__':
+    main()
