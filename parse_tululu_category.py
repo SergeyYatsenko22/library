5d8d4b8e90d2658517b3fc26edf0b0f8ae3283c7
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-06-13 20:26:32 +0300

    Parsing all pages with While

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,11 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-def parse_tululu_tutorial(content):
-    soup = BeautifulSoup(content.text, 'lxml')
-    # book_id = soup.find(class_='d_book').find('a')['href']
-    # return book_id
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
 
+def get_book_id(content):
+    soup = BeautifulSoup(content.text, 'lxml')
     books = soup.find_all(class_='d_book')
     for book in books:
         book_id = book.find('a')['href']
@@ -15,10 +16,28 @@ def parse_tululu_tutorial(content):
 
 page_response = requests.get('https://tululu.org/l55/')
 page_response.raise_for_status()
+get_book_id(page_response)
+
+for page_id in range (1, 11):
+    page_response = requests.get(f'https://tululu.org/l55/{page_id}')
+    page_response.raise_for_status()
+    get_book_id(page_response)
 
 
-book_id = parse_tululu_tutorial(page_response)
+# book_id = parse_tululu_tutorial(page_response)
 # book_url = urljoin('https://tululu.org/', book_id)
 
 # print(book_url)
-parse_tululu_tutorial(page_response)
+# parse_tululu_tutorial(page_response)
+
+
+
+# цикл по всей библиотеке по жанру Научная фантастика
+# while True:
+#     page_response = requests.get(f'https://tululu.org/l55/{page_id}')
+#     page_response.raise_for_status()
+#     if not check_for_redirect(page_response):
+#         parse_tululu_tutorial(page_response)
+#         page_id += 1
+#     else:
+#         break
